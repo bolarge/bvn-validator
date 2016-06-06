d968375d3368e44d4d655fc4e396a04cf0107e6c
Taiwo Orilogbon <tawo@one-cred.com>
2016-06-06 16:57:54 +0100

    - Test server without mean

--- app.js
+++ app.js
@@ -13,45 +13,51 @@ var mongoose = require('mongoose');
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
  */
-Bvn.register(function(app, auth, database, passport) {
+Bvn.register(function (app, auth, database, passport) {
 
-var User = mongoose.model('User');
+  var User = mongoose.model('User');
 
-    passport.use(new BasicStrategy(
-      function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-          if (err) { return done(err); }
-          if (!user) { return done(null, false); }
-          if (!user.authenticate(password)) { return done(null, false); }
-          return done(null, user);
-        });
-      }
-      ));
+  passport.use(new BasicStrategy(
+    function (username, password, done) {
+      User.findOne({username: username}, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!user.authenticate(password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    }
+  ));
 
   //We enable routing. By default the Package Object is passed to the routes
   Bvn.routes(app, auth, database, passport);
 
   /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    Bvn.settings({
+   //Uncomment to use. Requires meanio@0.3.7 or above
+   // Save settings with callback
+   // Use this for saving data from administration pages
+   Bvn.settings({
         'someSetting': 'some value'
     }, function(err, settings) {
         //you now have the settings object
     });
 
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    Bvn.settings({
+   // Another save settings example this time with no callback
+   // This writes over the last settings.
+   Bvn.settings({
         'anotherSettings': 'some value'
     });
 
-    // Get settings. Retrieves latest saved settigns
-    Bvn.settings(function(err, settings) {
+   // Get settings. Retrieves latest saved settigns
+   Bvn.settings(function(err, settings) {
         //you now have the settings object
     });
-    */
+   */
 
   return Bvn;
 });
--- server/routes/bvn.js
+++ server/routes/bvn.js
@@ -12,26 +12,26 @@ var parseString = require('xml2js').parseString;
 var checker = require("debug")("xml");
 
 
-var config = { 
-     /*           host: "http://196.6.103.58:8080/BVNValidationBooleanMatch/bvnValidation?wsdl"
- */
-host : "http://196.6.103.100/BVNValidationBooleanMatch/bvnValidation?wsdl"
-           };
+var config = {
+  /*           host: "http://196.6.103.58:8080/BVNValidationBooleanMatch/bvnValidation?wsdl"
+   */
+  host: "http://196.6.103.100/BVNValidationBooleanMatch/bvnValidation?wsdl"
+};
 
 var args = {
-                requestXML : '',
-                organisationCode : "002002"
-         };
+  requestXML: '',
+  organisationCode: "002002"
+};
 
 var username = "nimbss4real";
 var password = "great55&&31L990";
 
 
-var base = path.resolve(__dirname,"../","../") + "/node_modules/nibssSSM/keys/public.key";
+var base = path.resolve(__dirname, "../", "../") + "/node_modules/nibssSSM/keys/public.key";
 
-var nibss =  path.resolve(__dirname,"../","../") + "/node_modules/nibssSSM/keys/nibss.key";
+var nibss = path.resolve(__dirname, "../", "../") + "/node_modules/nibssSSM/keys/nibss.key";
 
- var wsdlOptions = {
+var wsdlOptions = {
   attribut: 'verifySingleBVN',
   valueKey: 'http://bmatch.bvn.nibss.com/',
   xmlKey: 'verifySingleBVN'
@@ -41,91 +41,91 @@ var nibss =  path.resolve(__dirname,"../","../") + "/node_modules/nibssSSM/keys/
 /**
  * * Simply generate the keys to be used
  * */
- function generateKey(username,password){
+function generateKey(username, password) {
   var deferred = Q.defer();
-  ssm.generateKey(username,password)
-  .then(function(res){
-    deferred.resolve(res);
-  },function(err){
-deferred.reject(err);
-});
+  ssm.generateKey(username, password)
+    .then(function (res) {
+      deferred.resolve(res);
+    }, function (err) {
+      deferred.reject(err);
+    });
 
   return deferred.promise;
 }
 
 
- function validateBVN(inputDataObject){
+function validateBVN(inputDataObject) {
 
   var deferred = Q.defer();
 
-  var thexml = js2xml("ValidationRequest",inputDataObject);
+  var thexml = js2xml("ValidationRequest", inputDataObject);
 
   ssm.encrypt(thexml, nibss)
-  .then(function(res){
-	args.requestXML = res;
-    var options = {
-       ignoredNamespaces: {
-         namespaces: ['xsi'],
-         override: true
-       }
-     };
+    .then(function (res) {
+      args.requestXML = res;
+      var options = {
+        ignoredNamespaces: {
+          namespaces: ['xsi'],
+          override: true
+        }
+      };
 
-    soap.createClient(config.host,options, function(err, soapClient) {
+      soap.createClient(config.host, options, function (err, soapClient) {
 
-      if(err)  deferred.reject(err);
+        if (err)  deferred.reject(err);
 
-      soapClient.verifySingleBVN(args,  function(err, soapResp){
+        soapClient.verifySingleBVN(args, function (err, soapResp) {
 
-        if(err)  deferred.reject(err);
-          
-        ssm.decrypt(soapResp.ValidationResponse, password, base)
-        .then(function(res){
-      
-        //console.log(res);    
-        parseString(res, function (err, result) {
-              
-              deferred.resolve(result);
-        });
-
-        },function(err){
-          debug(err);
+          if (err)  deferred.reject(err);
+          ssm.decrypt(soapResp.ValidationResponse, password, base)
+            .then(function (res) {
+              //console.log(res);
+              parseString(res, function (err, result) {
+                deferred.resolve(result);
+              });
+            }, function (err) {
+              debug(err);
+            });
         });
 
       });
-
     });
-  });
 
   return deferred.promise;
 }
 
-module.exports = function(Nimbss, app, auth, database, passport) {
-  
+module.exports = function (app, passport) {
 
-   app.post("/oapi/bvnValidation", 
-    passport.authenticate('basic', { session: false }),
-    function(req, res, next){
-      
+
+  app.post("/oapi/bvnValidation",
+    //passport.authenticate('basic', {session: false}),
+    function (req, res, next) {
+
       var inputDataObject = req.body.inputDataObject;
 
       validateBVN(inputDataObject)
-       .then(function(re){ res.send(re);},function(err){
-	res.status(500).send(err);
-});
+        .then(function (re) {
+          res.send(re);
+        }, function (err) {
+          res.status(500).send(err);
+        });
 
-   });
+    });
 
 
-    app.post("/oapi/generateKeys", 
-      passport.authenticate('basic', { session: false }),
-      function(req, res, next){
-                                   
+  app.post("/oapi/generateKeys",
+    //passport.authenticate('basic', {session: false}),
+    function (req, res, next) {
+
       password = req.body.password;
 
+      console.log("Body: ", req.body);
       generateKey(req.body.username, req.body.password)
-           .then(function(re){ res.send(re);},function(err){
-res.status(500).send(err);
-});
+        .then(function (re) {
+          res.send(re);
+        }, function (err) {
+          res.status(500).send(err);
+        });
 
-   });
+    });
 };
