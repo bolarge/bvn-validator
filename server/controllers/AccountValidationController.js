b90f0b68cddf9d41c6202e0b4a12693c63997ba4
Temidayo Yembra <temi@ip-192-168-1-37.us-west-2.compute.internal>
2016-10-07 16:53:29 +0100

    OCL-200: NIP account validation

--- server/controllers/AccountValidationController.js
+++ server/controllers/AccountValidationController.js
@@ -4,7 +4,13 @@
 
 "use strict";
 
-var Agent = require("socks5-https-client/lib/Agent"),
+var Agent = require('socks5-https-client/lib/Agent'),
+  soap = require('soap'),
+  moment = require('moment'),
+  Q = require('q'),
+  ssm = require('../lib/ssm'),
+  js2Xml = require('js2xmlparser'),
+  parseString = require('xml2js').parseString,
   request = require('request'),
   config = require('../../config'),
   _ = require('lodash'),
@@ -13,7 +19,25 @@ var Agent = require("socks5-https-client/lib/Agent"),
   q = require('q');
 
 const STATUS_SUCCESS = "00";
-const STATUS_RECORD_NOT_FOUND = "02";
+const STATUS_RECORD_NOT_FOUND = "21";
+
+
+var soapClient;
+
+soap.createClient(config.nibss.nip.wsdl, {
+  ignoredNamespaces: {
+    namespaces: [],
+    override: true
+  }
+}, function (err, client) {
+
+  if (err) {
+    console.error('Could not initialize Soap Client!');
+    throw err;
+  }
+  console.log('Soap client intialization completed!');
+  soapClient = client;
+});
 
 
 var validateRequest = function (data, requiredFields) {
@@ -112,7 +136,7 @@ var performAccountValidation = function (request) {
       }
 
       console.log('Calling service...');
-      return accountService(request)
+      return nipAccountService(request)
         .then(function (result) {
           if (!result) {
             throw new Error('RESULT_NOT_FOUND');
@@ -185,6 +209,73 @@ var accountService = function (data) {
 
 };
 
+var nipAccountService = function (data) {
+
+  var args = {};
+
+  var request = {
+    SessionID: config.nibss.nip.schemeCode + moment().format('YYMMDDHHmmss') + "123456789012",
+    DestinationInstitutionCode: data.bankCode,
+    ChannelCode: config.nibss.nip.channelCode,
+    AccountNumber: data.accountNumber
+  };
+
+  var deferred = Q.defer();
+
+  var xmlRequest = js2Xml("NESingleRequest", request);
+
+  if (!soapClient) {
+    throw new Error('Soap client is still initializing');
+  }
+
+  console.log('Encrypting request...');
+  ssm.encrypt(xmlRequest, config.ssm.nibssKeyPath)
+    .then(function (response) {
+      console.log(response);
+      args.request = response;
+      console.log('Request has been encrypted');
+
+      var timeStart = Date.now();
+      console.log('Starting verify request...');
+      soapClient.nameenquirysingleitem(args, function (err, soapResp) {
+        console.log('Verify request completed after:' + (Date.now() - timeStart) + "ms");
+
+        if (err) {
+          return deferred.reject(err);
+        }
+
+        if (!soapResp) {
+          return deferred.reject(new Error("Empty response returned from Validation request."));
+        }
+
+        console.log('Decrypting response');
+        ssm.decrypt(soapResp.return, config.nibss.nip.password, config.nibss.nip.privateKeyPath)
+          .then(function (res) {
+            console.log('Response decrypted successfully. Parsing...');
+
+            parseString(res, function (err, result) {
+
+              var response = result.NESingleResponse;
+              var obj = {
+                bankCode: response.DestinationInstitutionCode[0],
+                bvn: response.BankVerificationNumber[0],
+                surname: "",
+                otherNames: response.AccountName[0],
+                accountNumber: response.AccountNumber[0],
+                status: response.ResponseCode[0]
+              };
+
+              deferred.resolve(obj);
+            });
+          }, function (err) {
+            debug(err);
+          });
+      });
+    });
+
+  return deferred.promise;
+
+};
 
 module.exports.validateAccount = function (req, res) {
   console.log("Starting account validation...");
