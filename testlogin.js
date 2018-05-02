3e3061a70a6a6eeff811ed8794e0c7f18d9da3ac
Nonami Animashaun <nanimashaun@onefi.co>
2018-05-02 13:41:24 +0100

    Experiment continua

--- testlogin.js
+++ testlogin.js
@@ -1,3 +1,8 @@
 /**
  * Created by nonami on 02/05/2018.
  */
+const NIBSS = require('./server/services/BVNProvider/nibss');
+
+NIBSS.login()
+  .then((cookie) => console.log(cookie))
+  .finally(() => process.exit());
