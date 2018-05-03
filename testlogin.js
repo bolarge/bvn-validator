aa61258cf94dd929f21f27b6a254d755fb5084af
Nonami Animashaun <nanimashaun@onefi.co>
2018-05-03 18:44:35 +0100

    Deleted unused file

--- testlogin.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nonami on 02/05/2018.
- */
-const NIBSS = require('./server/services/BVNProvider/nibss');
-
-NIBSS.login()
-  .then((cookie) => console.log(cookie))
-  .finally(() => process.exit());
