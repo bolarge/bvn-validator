d02472056de7ffc7036459157f4607eca43fa75c
Nonami Animashaun <nanimashaun@onefi.co>
2020-03-24 21:33:34 +0100

    CP-296
    Removed unused endpoints, functions and dependencies.

--- server/routes/bvn.js
+++ server/routes/bvn.js
@@ -7,23 +7,6 @@ var config = require('../../config'),
 
 module.exports = function (app) {
 
-    app.post("/oapi/bvnValidation",
-        require('passport').authenticate('basic', {session: false}),
-        BvnController.validate
-    );
-
-
-    app.post("/oapi/validate",
-        require('passport').authenticate('basic', {session: false}),
-        BvnController.validateBoolean
-    );
-
-
-    app.get("/oapi/validate",
-        require('passport').authenticate('basic', {session: false}),
-        BvnController.validateBoolean
-    );
-
     app.get("/oapi/resolveBvn/:bvn",
         require('passport').authenticate('basic', {session: false}),
         BvnController.resolveBvn
---
+++ server/services/NibssPhoneDataService.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nonami on 16/02/2020.
+ */
